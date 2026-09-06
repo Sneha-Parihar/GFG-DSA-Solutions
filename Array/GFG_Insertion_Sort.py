10a3f67ed50b610a6dc50ac314e5c1af535d4cf2
Sneha Parihar <snehaparihar246@gmail.com>
2026-09-06 22:43:36 +0530

    Add insertion sort solution

---
+++ Array/GFG_Insertion_Sort.py
@@ -0,0 +1,12 @@
+class Solution:
+    def insertionSort(self, arr):
+        # code here
+        n=len(arr)
+        for i in range(1,n) :
+              temp=arr[i]
+              j=i-1
+              while j>=0 and arr[j]>temp :
+                   arr[j+1]=arr[j]
+                   j-=1
+              arr[j+1]=temp
+        return arr
